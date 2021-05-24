0b8ddc7eb382ad33f6302939413574e4919a8a42
earjcr@msn.com <earjcr@msn.com>
2021-05-24 08:33:34 -0400

    Daily save 5/23/21
    Small progress toward POSTing the purchase info and getting order #

--- cart.js
+++ cart.js
@@ -15,13 +15,17 @@ string = string + '"name":"'   + choiceName  + '",'
 string = string + '"color":"'  + choiceColor + '",'
 string = string + '"price":"'  + choicePrice + '",'
 string = string + '"_id":"'    + choiceId    + '"}'
+console.log(string);
+
+// Initialize the valuesChecked variable
+const valuesChecked = 0
 
 // Create product array if it does not yet exist, and add the choiceId to the array
-if (!sessionStorage['cartProduct']) {
+// if (!sessionStorage['cartProduct']) {
     sessionStorage.setItem('cartProduct', string);
-} else {
+// } else {
 // NEED TO ADD THE PROGRAMMING FOR MULTIPLE ITEMS
-}
+// }
 
 // sessionStorage.setItem('cartProduct', (sessionStorage.getItem('cartProduct')) && choiceId);
 
@@ -42,8 +46,8 @@ editCart = () => {
         totalPrice = totalPrice + choicePrice
         addTotal.textContent = totalPrice / 100;
 
-    // Second Card Elements asking color choice
-
+    // Second card gets information for the purchasor
+    console.log('cart.js[53]')
     // Get customer info after clicking checkout button
     const btnCheckout = document.getElementById('btnCheckout')
     btnCheckout.addEventListener('click', () => {
@@ -55,37 +59,45 @@ editCart = () => {
         const city      = document.getElementById('city'     )
         const email     = document.getElementById('email'    )
         // const postal    = document.getElementById('postal'   )
-
+        console.log('cart.js[65]')
         // Create the stringify'd customer info for POSTing to the API 
         let stringPost = '"contact":{'
-        stringPost = stringPost + '"firstName:"'  + firstName.value + '",'
-        stringPost = stringPost + '"lastName":"'  + lastName.value  + '",'
-        stringPost = stringPost + '"address":"'   + address.value   + '",'
-        stringPost = stringPost + '"city":"'      + city.value      + '",'
-        stringPost = stringPost + '"email":"'     + email.value     + '"}'
+        console.log(stringPost)
+        stringPost = stringPost + '"firstName:"'  + firstName.textContent + '",'
+        console.log(stringPost)
+        stringPost = stringPost + '"lastName":"'  + lastName.textContent  + '",'
+        console.log(stringPost)
+        stringPost = stringPost + '"address":"'   + address.textContent   + '",'
+        console.log(stringPost)
+        stringPost = stringPost + '"city":"'      + city.textContent      + '",'
+        console.log(stringPost)
+        stringPost = stringPost + '"email":"'     + email.textContent     + '"}'
+        console.log(stringPost)
         stringPost = stringPost + '"products":["' + choiceId        + '"]'
+        console.log(stringPost)
         sessionStorage.setItem('stringPost', stringPost);
-
-        // * Expects request to contain:
-        let contact = {
-            firstName: firstName.value,
-            lastName: lastName.value,
-            address: address.value,
-            city: city.value,
-            email: email.value}
-        let products = choiceId;
-        const object = contact, products;
+        console.log('cart.js[74]')
+        // // * Expects request to contain:
+        // let contact = {
+        //     firstName: firstName.value,
+        //     lastName: lastName.value,
+        //     address: address.value,
+        //     city: city.value,
+        //     email: email.value}
+        // let productIds = choiceId;
+        // const object = contact
+        // object = object, productIds;
+        submit();
     })
 }
-editCart();
-
 // API export function
 makeRequest = (data) => {
     return new Promise((resolve, reject) => {
         let apiRequest = new XMLHttpRequest();
         apiRequest.open('POST', 'http://localhost:3000/api/teddies/order');
         apiRequest.setRequestHeader('Content-Type', 'application/json');
-        apiRequest.send(JSON.stringify(data));
+        // apiRequest.send(JSON.stringify(data));
+        apiRequest.send(data);
         apiRequest.onreadystatechange = () => {
             if (apiRequest.readyState === 4) {
                 if (apiRequest.status === 201) {
@@ -94,6 +106,7 @@ makeRequest = (data) => {
                 }
                 if (apiRequest.status === 400) {
                     // Unsuccessful
+                    console.log(apiRequest.status)
                     reject('Error - API Request unsuccessful');
                 }
             }
@@ -101,18 +114,21 @@ makeRequest = (data) => {
     });
 }
 
+// const object = sessionStorage.getItem('stringPost');
+
+editCart();
 
 submit = async () => {
     try {
         // Run makeRequest and wait for a response
-        const requestPromise = makeRequest(object);
+        const requestPromise = makeRequest(stringPost);
         const response = await requestPromise;
         // Display response
 
     }   catch (error) {
         // Failed request
         document.querySelector('form').innerHTML = '<h2 class = "mx-auto">' + error + '<h2>';
-    // }
+    }
 }
 
-submit();
+// submit();
