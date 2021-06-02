8256561b719f9c4de47b99108a8569cc94eee02a
earjcr@msn.com <earjcr@msn.com>
2021-06-02 11:12:26 -0400

    Mid-day save - 6/2/21
    Started overhaul of Cart Contents card with removal embedded

--- cart.js
+++ cart.js
@@ -3,25 +3,28 @@
 // Reset the indicator
 sessionStorage.setItem('colorChosen', false);
 
-// Import the product data of the newly chosen bear
-const choiceId          = sessionStorage.getItem('choiceId');
-const choiceName        = sessionStorage.getItem('choiceName');
-const choiceColor       = sessionStorage.getItem('choiceColor');
-const choicePrice       = sessionStorage.getItem('choicePrice');
-
 // Create sessionStorage variable named 'cart', if doesn't exist, and append newly chosen bear
 createCart = () => {
+  // Import the product data of the newly chosen bear
+  const choiceId          = sessionStorage.getItem('choiceId');
+  const choiceName        = sessionStorage.getItem('choiceName');
+  const choiceColor       = sessionStorage.getItem('choiceColor');
+  const choicePrice       = sessionStorage.getItem('choicePrice');
+
+  // If cart exists, assign variable. If not, create it
   let cart = sessionStorage.getItem('cart');
   if (!sessionStorage['cart']) {
      cart = []
-   } else {
+  } else {
      cart = JSON.parse(cart);     
-   };
+  };
+
+  // Append the newly chosen bear data to the cart
   let choice = {
-      name:  choiceName,
-      id:    choiceId,
-      price: choicePrice,
-      color: choiceColor,
+    id:    choiceId,
+    name:  choiceName,
+    color: choiceColor,
+    price: choicePrice,
   };
   cart.push(choice);
   sessionStorage.setItem('cart', JSON.stringify(cart));
@@ -32,28 +35,72 @@ createCart ();
 // Initialize variables
 let orderId;
 const valuesChecked = 0
-let totalPrice = 0
 
 displayCart = () => {
-  // First Card Elements: showing the chosen bear info and cart contents
-  
-
+  // First Card Elements: showing the Cart Contents
   let cart = JSON.parse(sessionStorage.getItem('cart'));
-  for (let j = 0; j < cart.length; j++) {
 
-    // Add product to Cart Contents
-    const addName = document.getElementById('addName');
-        addName.textContent = choiceName + ' - ' + choiceColor;
-    const addPrice = document.getElementById('addPrice');
-        addPrice.textContent = choicePrice / 100;
+  // Assign parent ul columns for Name, Price, and Remove
+  const addName   = document.getElementById('addName');
+  const addPrice  = document.getElementById('addPrice');
+  const addRemove = document.getElementById('addRemove');
 
-    // Total price will need to move for programming multiple purchase
-    const addTotal = document.getElementById('totalPrice');
-        totalPrice = totalPrice + choicePrice;
-        addTotal.textContent = totalPrice / 100;
+  // Remove all children from ul's
+  while (addName.hasChildNodes()) {  
+    addName.removeChild(addName.firstChild);
+    addPrice.removeChild(addPrice.firstChild);
+    addRemove.removeChild(addRemove.firstChild);
   }
+  // Build up the Cart Contents, starting with the column header
+  let newName = document.createElement('li');
+      newName.textContent = 'Bear'
+      newName.classList.add('list-inline-item');
+      addName.appendChild(newName);
+  let newPrice = document.createElement('li');
+      newPrice.textContent = 'Price'
+      newPrice.classList.add('list-inline-item');
+      newPrice.appendChild(newPrice);
+  let newRemove = document.createElement('li');
+      newRemove.textContent = 'Remove'
+      newRemove.classList.add('list-inline-item');
+      addRemove.appendChild(newRemove);
 
-    // Second Card Elements: form information for the purchasor
+  // Create 'Remove' button that will be replicated
+  let btnRemove = document.createElement('button');
+      btnRemove.textContent = 'X';
+      btnRemove.classList.add('btn btn-danger list-inline'); 
+
+  let totalPrice = 0;
+  let splice = null
+
+  // Loop through to add each bear info to Cart Contents
+  for (let j = 0; j < cart.length; j++) {
+      let remove = btnRemove;
+      remove.addEventListener('click', () => {
+        spliceNumber = j;
+        continue;
+      });
+      remove.setAttribute('type', 'button');
+      addRemove.appendChild(remove);
+
+      newName.textContent = cart.name[j] + ' - ' + cart.color[j];
+      addName.appendChild(newName);
+
+      newPrice.textContent = cart.price[j] / 100;
+      addPrice.appendChild(newPrice);
+      totalPrice = totalPrice + cart.price[j];
+
+    }    
+    newName.textContent = 'Total Price';
+    addName.appendChild(newName);
+
+    newPrice.textContent = totalPrice / 100;
+    addPrice.appendChild(newPrice);
+  
+    cart.splice(spliceNumber,1)
+    sessionStorage.setItem("cart", JSON.stringify(cart));
+
+  // Second Card Elements: form information for the purchasor
     console.log('cart.js[50]');
     // Get customer info after clicking checkout button
     const btnCheckout = document.getElementById('btnCheckout');
